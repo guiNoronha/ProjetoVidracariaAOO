1d3ed7066b3a79ac8011e7ab8fab4168e371a7d1
Andre <and.zucolotto@gmail.com>
2018-10-23 20:44:19 -0300

    tela casdastro produto

---
+++ ProjetoVidracariaAOO/Form_Produto.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace ProjetoVidracariaAOO
+{
+    partial class Form_Produto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtProdDesc = new System.Windows.Forms.TextBox();
+            this.lblDescProd = new System.Windows.Forms.Label();
+            this.txtIdProd = new System.Windows.Forms.TextBox();
+            this.lblProdutoId = new System.Windows.Forms.Label();
+            this.txtPrecoPord = new System.Windows.Forms.TextBox();
+            this.lblPrecoProd = new System.Windows.Forms.Label();
+            this.txtEstoqueProd = new System.Windows.Forms.TextBox();
+            this.lblProdutoEstoque = new System.Windows.Forms.Label();
+            this.lblNomeProduto = new System.Windows.Forms.Label();
+            this.txtNomeProd = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblCadClie = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnListarProd = new System.Windows.Forms.Button();
+            this.btnCriarProd = new System.Windows.Forms.Button();
+            this.btnVoltar = new System.Windows.Forms.PictureBox();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnVoltar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtProdDesc
+            // 
+            this.txtProdDesc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtProdDesc.Location = new System.Drawing.Point(177, 223);
+            this.txtProdDesc.Name = "txtProdDesc";
+            this.txtProdDesc.Size = new System.Drawing.Size(359, 26);
+            this.txtProdDesc.TabIndex = 44;
+            // 
+            // lblDescProd
+            // 
+            this.lblDescProd.AutoSize = true;
+            this.lblDescProd.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescProd.Location = new System.Drawing.Point(68, 223);
+            this.lblDescProd.Name = "lblDescProd";
+            this.lblDescProd.Size = new System.Drawing.Size(103, 23);
+            this.lblDescProd.TabIndex = 43;
+            this.lblDescProd.Text = "Descrição";
+            // 
+            // txtIdProd
+            // 
+            this.txtIdProd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIdProd.Location = new System.Drawing.Point(102, 138);
+            this.txtIdProd.Name = "txtIdProd";
+            this.txtIdProd.Size = new System.Drawing.Size(116, 26);
+            this.txtIdProd.TabIndex = 42;
+            // 
+            // lblProdutoId
+            // 
+            this.lblProdutoId.AutoSize = true;
+            this.lblProdutoId.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProdutoId.Location = new System.Drawing.Point(68, 138);
+            this.lblProdutoId.Name = "lblProdutoId";
+            this.lblProdutoId.Size = new System.Drawing.Size(28, 23);
+            this.lblProdutoId.TabIndex = 41;
+            this.lblProdutoId.Text = "ID";
+            // 
+            // txtPrecoPord
+            // 
+            this.txtPrecoPord.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPrecoPord.Location = new System.Drawing.Point(241, 180);
+            this.txtPrecoPord.Name = "txtPrecoPord";
+            this.txtPrecoPord.Size = new System.Drawing.Size(294, 26);
+            this.txtPrecoPord.TabIndex = 40;
+            // 
+            // lblPrecoProd
+            // 
+            this.lblPrecoProd.AutoSize = true;
+            this.lblPrecoProd.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrecoProd.Location = new System.Drawing.Point(68, 183);
+            this.lblPrecoProd.Name = "lblPrecoProd";
+            this.lblPrecoProd.Size = new System.Drawing.Size(139, 23);
+            this.lblPrecoProd.TabIndex = 39;
+            this.lblPrecoProd.Text = "Preco Produto";
+            // 
+            // txtEstoqueProd
+            // 
+            this.txtEstoqueProd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEstoqueProd.Location = new System.Drawing.Point(325, 138);
+            this.txtEstoqueProd.Name = "txtEstoqueProd";
+            this.txtEstoqueProd.Size = new System.Drawing.Size(211, 26);
+            this.txtEstoqueProd.TabIndex = 36;
+            // 
+            // lblProdutoEstoque
+            // 
+            this.lblProdutoEstoque.AutoSize = true;
+            this.lblProdutoEstoque.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProdutoEstoque.Location = new System.Drawing.Point(237, 141);
+            this.lblProdutoEstoque.Name = "lblProdutoEstoque";
+            this.lblProdutoEstoque.Size = new System.Drawing.Size(82, 23);
+            this.lblProdutoEstoque.TabIndex = 35;
+            this.lblProdutoEstoque.Text = "Estoque";
+            // 
+            // lblNomeProduto
+            // 
+            this.lblNomeProduto.AutoSize = true;
+            this.lblNomeProduto.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNomeProduto.Location = new System.Drawing.Point(68, 94);
+            this.lblNomeProduto.Name = "lblNomeProduto";
+            this.lblNomeProduto.Size = new System.Drawing.Size(142, 23);
+            this.lblNomeProduto.TabIndex = 32;
+            this.lblNomeProduto.Text = "Nome Produto";
+            // 
+            // txtNomeProd
+            // 
+            this.txtNomeProd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNomeProd.Location = new System.Drawing.Point(216, 94);
+            this.txtNomeProd.Name = "txtNomeProd";
+            this.txtNomeProd.Size = new System.Drawing.Size(320, 26);
+            this.txtNomeProd.TabIndex = 31;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(49)))), ((int)(((byte)(66)))));
+            this.panel2.Controls.Add(this.lblCadClie);
+            this.panel2.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.panel2.Location = new System.Drawing.Point(2, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(624, 61);
+            this.panel2.TabIndex = 45;
+            // 
+            // lblCadClie
+            // 
+            this.lblCadClie.AutoSize = true;
+            this.lblCadClie.Font = new System.Drawing.Font("Century Gothic", 26.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCadClie.Location = new System.Drawing.Point(93, 9);
+            this.lblCadClie.Name = "lblCadClie";
+            this.lblCadClie.Size = new System.Drawing.Size(382, 42);
+            this.lblCadClie.TabIndex = 0;
+            this.lblCadClie.Text = "Cadastro de Produto";
+            this.lblCadClie.Click += new System.EventHandler(this.lblCadClie_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(49)))), ((int)(((byte)(66)))));
+            this.panel1.Controls.Add(this.btnListarProd);
+            this.panel1.Controls.Add(this.btnCriarProd);
+            this.panel1.Controls.Add(this.btnVoltar);
+            this.panel1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.Location = new System.Drawing.Point(-4, 277);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(623, 61);
+            this.panel1.TabIndex = 46;
+            // 
+            // btnListarProd
+            // 
+            this.btnListarProd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnListarProd.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnListarProd.Location = new System.Drawing.Point(268, 12);
+            this.btnListarProd.Name = "btnListarProd";
+            this.btnListarProd.Size = new System.Drawing.Size(87, 36);
+            this.btnListarProd.TabIndex = 4;
+            this.btnListarProd.Text = "Listar";
+            this.btnListarProd.UseVisualStyleBackColor = true;
+            // 
+            // btnCriarProd
+            // 
+            this.btnCriarProd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCriarProd.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnCriarProd.Location = new System.Drawing.Point(508, 12);
+            this.btnCriarProd.Name = "btnCriarProd";
+            this.btnCriarProd.Size = new System.Drawing.Size(87, 36);
+            this.btnCriarProd.TabIndex = 3;
+            this.btnCriarProd.Text = "Criar";
+            this.btnCriarProd.UseVisualStyleBackColor = true;
+            // 
+            // btnVoltar
+            // 
+            this.btnVoltar.Image = global::ProjetoVidracariaAOO.Properties.Resources.back;
+            this.btnVoltar.Location = new System.Drawing.Point(12, 15);
+            this.btnVoltar.Name = "btnVoltar";
+            this.btnVoltar.Size = new System.Drawing.Size(35, 33);
+            this.btnVoltar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnVoltar.TabIndex = 2;
+            this.btnVoltar.TabStop = false;
+            // 
+            // Form_Produto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(604, 341);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.txtProdDesc);
+            this.Controls.Add(this.lblDescProd);
+            this.Controls.Add(this.txtIdProd);
+            this.Controls.Add(this.lblProdutoId);
+            this.Controls.Add(this.txtPrecoPord);
+            this.Controls.Add(this.lblPrecoProd);
+            this.Controls.Add(this.txtEstoqueProd);
+            this.Controls.Add(this.lblProdutoEstoque);
+            this.Controls.Add(this.lblNomeProduto);
+            this.Controls.Add(this.txtNomeProd);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form_Produto";
+            this.Text = "Form_Produto";
+            this.Load += new System.EventHandler(this.Form_Produto_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.btnVoltar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtProdDesc;
+        private System.Windows.Forms.Label lblDescProd;
+        private System.Windows.Forms.TextBox txtIdProd;
+        private System.Windows.Forms.Label lblProdutoId;
+        private System.Windows.Forms.TextBox txtPrecoPord;
+        private System.Windows.Forms.Label lblPrecoProd;
+        private System.Windows.Forms.TextBox txtEstoqueProd;
+        private System.Windows.Forms.Label lblProdutoEstoque;
+        private System.Windows.Forms.Label lblNomeProduto;
+        private System.Windows.Forms.TextBox txtNomeProd;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblCadClie;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnListarProd;
+        private System.Windows.Forms.Button btnCriarProd;
+        private System.Windows.Forms.PictureBox btnVoltar;
+    }
+}
---
+++ ProjetoVidracariaAOO/Form_Produto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoVidracariaAOO
+{
+    public partial class Form_Produto : Form
+    {
+        public Form_Produto()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_Produto_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblCadClie_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
